a2b4f0971ba0ca0753f3b53c24d5901dce39e780
itsmerawbby <60546046+zahra-ameilia1@users.noreply.github.com>
2021-09-30 21:52:59 +0700

    Update Mahasiswa.php

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -5,8 +5,8 @@ class Mahasiswa extends Controller {
         $data['judul'] = 'Daftar Mahasiswa';
         $data['mhs'] = $this->model('Mahasiswa_model')->getAllMahasiswa();
 
-        $this->view('templates/header');
-        $this->view('mahasiswa/index');
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/index', $data);
         $this->view('templates/footer');
     }
 }
