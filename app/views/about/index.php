2ac6efeb0eefd99cf7779f3779fdb83f24000385
itsmerawbby <60546046+zahra-ameilia1@users.noreply.github.com>
2021-09-30 20:21:50 +0700

    Latihan Video 6 Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,6 @@
-
-        <h1>About Me</h1>
+<div class="container">
+        <h1 class="mt-3">About Me</h1>
+        <img src="<?= BASEURL; ?>/img/me.jpg" alt="Zahra Dwi Ameilia" width="200" class="rounded-circle">
         <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, 
         saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,2 +1,8 @@
-
-        <h1>Selamat Datang di Website Saya</h1>
+<div class="container">
+        <div class="jumbotron mt-3">
+                <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+                <p class="lead">Ini adalah halaman awal dari website saya.</p>
+                <hr class="my_4"></hr>
+                <a class="btn btn-primary btn-lg" href="" role="button">Let's Go!</a>
+        </div>
+</div>
